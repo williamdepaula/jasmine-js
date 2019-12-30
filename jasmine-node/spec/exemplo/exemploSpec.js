421879d269ab950d091cf2df4e4acd42ce7a1843
William de Paula <william.paula@azi.com.br>
2019-12-30 14:14:27 -0400

    fim da seção 8.

--- jasmine-node/spec/exemplo/exemploSpec.js
+++ jasmine-node/spec/exemplo/exemploSpec.js
@@ -3,4 +3,8 @@ describe("Suite de teste de exemplo com NodeJS", function() {
 	it("deve demostrar o uso do Jasmine com o NodeJS", function() {
 		expect(true).toEqual(true);
 	});
+
+	it("deve demostrar o uso do karma em modo de escuta", function(){
+		expect(1).toEqual(1);
+	});
 });
